0c19e4ac12a737d855d36c7f6e4fd075d7243885
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-09-20 15:56:58 +0900

    $ bundle gem acts_as_shellscript_executable

---
+++ lib/acts_as_shellscript_executable.rb
@@ -0,0 +1,5 @@
+require "acts_as_shellscript_executable/version"
+
+module ActsAsShellscriptExecutable
+  # Your code goes here...
+end
---
+++ lib/acts_as_shellscript_executable/version.rb
@@ -0,0 +1,3 @@
+module ActsAsShellscriptExecutable
+  VERSION = "0.0.1"
+end
