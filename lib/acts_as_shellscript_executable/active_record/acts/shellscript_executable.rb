bbc3c7d17bb01eaf1ae96edfc3cf632dae54d98d
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-09-21 00:10:47 +0900

    recieve args

--- lib/acts_as_shellscript_executable/active_record/acts/shellscript_executable.rb
+++ lib/acts_as_shellscript_executable/active_record/acts/shellscript_executable.rb
@@ -7,21 +7,31 @@ module ActiveRecord
 
       module ClassMethods
         def acts_as_shellscript_executable(options = {})
-          configuration = { script: :script, stdout: nil }
-          configuration.update(options) if options.is_a?(Hash)
+          @@configuration = { script: :script, stdout: nil }
+          @@configuration.update(options) if options.is_a?(Hash)
 
           class_eval <<-EOV
             include ::ActiveRecord::Acts::ShellscriptExecutable::InstanceMethods
 
           EOV
         end
+
+        def configuration
+          @@configuration
+        end
       end
 
       module InstanceMethods
         def execute!
-          retval = %x(#{self.script})
+          script = configuration[:script]
+          retval = %x(#{self.send script})
           self.result = retval
           self.save!
+        end
+
+        private
+        def configuration
+          self.class.configuration
         end
       end
     end
--- spec/features/acts_as_shellscript_executable_spec.rb
+++ spec/features/acts_as_shellscript_executable_spec.rb
@@ -1,26 +1,49 @@
 require 'spec_helper'
 
-describe ActsAsShellscriptExecutable do
-  before do
-    ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
-    ActiveRecord::Schema.verbose = false
-    ActiveRecord::Base.connection.schema_cache.clear!
-    ActiveRecord::Schema.define(version: 1) do
-      create_table :scripts do |t|
-        t.column :script, :string
-        t.column :result, :string
+describe ActiveRecord::Base do
+  describe '.acts_as_shellscript_executable' do
+    before do
+      ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
+      ActiveRecord::Schema.verbose = false
+      ActiveRecord::Base.connection.schema_cache.clear!
+      ActiveRecord::Schema.define(version: 1) do
+        create_table :scripts do |t|
+          t.column :script,  :string
+          t.column :script2, :string
+          t.column :result,  :string
+        end
       end
     end
 
-    class Script < ActiveRecord::Base
-      acts_as_shellscript_executable script: :script
-    end
-  end
+    context 'given option {script: :script}' do
+      before do
+        class Script < ActiveRecord::Base
+          acts_as_shellscript_executable script: :script
+        end
+      end
 
-  it do
-    script = Script.create
-    script.script = 'echo "lalala"'
-    expect{script.execute!}.to \
-      change{script.result}.from(nil).to("lalala\n")
+      it do
+        script = Script.create
+        script.script = 'echo "lalala"'
+        expect{script.execute!}.to \
+          change{script.result}.from(nil).to("lalala\n")
+      end
+    end
+
+    context 'given option {script: :script2}' do
+      before do
+        class Script < ActiveRecord::Base
+          acts_as_shellscript_executable script: :script2
+        end
+      end
+
+      it do
+        script = Script.create
+        script.script  = 'echo "hehehe"'
+        script.script2 = 'echo "lalala"'
+        expect{script.execute!}.to \
+          change{script.result}.from(nil).to("lalala\n")
+      end
+    end
   end
 end
