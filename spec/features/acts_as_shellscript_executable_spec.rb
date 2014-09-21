3bfb7b0f45bf9f57329d371a12352aad12abf37a
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-09-21 11:39:00 +0900

    Multiple line

--- spec/features/acts_as_shellscript_executable_spec.rb
+++ spec/features/acts_as_shellscript_executable_spec.rb
@@ -119,5 +119,20 @@ describe ActiveRecord::Base do
           change{script.result}.from(nil).to("1\n")
       end
     end
+
+    context 'given option {script: "echo 1\necho 2", stdout: :result}' do
+      before do
+        class Script < ActiveRecord::Base
+          acts_as_shellscript_executable script: "echo 1\necho 2", stdout: :result
+        end
+      end
+
+      it do
+        script = Script.create
+        script.script  = 'echo "hehehe"'
+        expect{script.execute!}.to \
+          change{script.result}.from(nil).to("1\n2\n")
+      end
+    end
   end
 end
