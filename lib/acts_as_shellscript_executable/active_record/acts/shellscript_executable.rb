82babbc67fa679264169cefbdcea60944dc0a7fe
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-09-21 11:33:46 +0900

    Refactor

--- lib/acts_as_shellscript_executable/active_record/acts/shellscript_executable.rb
+++ lib/acts_as_shellscript_executable/active_record/acts/shellscript_executable.rb
@@ -40,12 +40,12 @@ module ActiveRecord
         end
 
         def execute(script, stdout)
-          case script
-          when Symbol
-            retval = %x(#{send script})
-          when String
-            retval = %x(#{script})
-          end
+          retval = case script
+                   when Symbol
+                     %x(#{send script})
+                   when String
+                     %x(#{script})
+                   end
           if stdout && respond_to?("#{stdout}=".to_sym)
             send("#{stdout}=".to_sym, retval)
           end
