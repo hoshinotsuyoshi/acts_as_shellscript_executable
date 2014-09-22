7b8f7b553c4578b79610a8e66fabfc9c1374131a
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-09-23 05:54:38 +0900

    Rubocop lib/acts_as_shellscript_executable/active_record/acts/shellscript_executable.rb

--- lib/acts_as_shellscript_executable/active_record/acts/shellscript_executable.rb
+++ lib/acts_as_shellscript_executable/active_record/acts/shellscript_executable.rb
@@ -13,7 +13,7 @@ module ActiveRecord
           configuration.update(options) if options.is_a?(Hash)
 
           class_eval <<-EOV
-            def #{configuration[:method].to_s}(&block)
+            def #{configuration[:method]}(&block)
               script = @@__configuration__[:script]
               answer = ''
               if @@__configuration__[:parallel]
@@ -27,26 +27,22 @@ module ActiveRecord
             end
           EOV
 
-          self.class_variable_set(:@@__configuration__, configuration)
+          class_variable_set(:@@__configuration__, configuration)
           include ::ActiveRecord::Acts::ShellscriptExecutable::InstanceMethods
         end
       end
 
       module InstanceMethods
         private
-        def __execute__(script, answer, block=nil)
-          script = case script
-                   when Symbol
-                     send script
-                   when String
-                     script
-                   end
+
+        def __execute__(script, answer, block = nil)
+          script = send script if script.is_a? Symbol
           retval = []
           script.split("\n").each do |line|
             if block
-              block.call( %x(#{line}))
+              block.call `#{line}`
             else
-              retval << %x(#{line})
+              retval << `#{line}`
             end
           end
           answer.replace retval.join
